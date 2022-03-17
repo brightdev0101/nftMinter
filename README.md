e3d59e7b92fba597d2ed5de9d203e558a796fdd6
brightdev0101 <brightdev0101@gmail.com>
2022-03-16 16:46:51 -0800

    second commit

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ In this beginner-level tutorial, we'll teach you how to connect your smart contr
 ## 🤔 Note
 
 Neither the `minter-starter-files` nor the `nft-tutorial` have their `node_modules` folder. So before running `npm start` in your terminal to start the React project, you need to run `npm install` to download all the package dependencies.
+
+nft-key information
+ API Key: 1ee2441712c56f9e1e34
+ API Secret: 31e975615c14e0f38758642a5f9c789308c7b11a4d62d1aedc1ff9b2bd6509fd
+ JWT: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiI0NGI4M2Q1Ny03ODBiLTRiZDUtOTU2NS1kNTg4Mzc1YjRlZWIiLCJlbWFpbCI6ImJyaWdodGRldjAxMDFAb3V0bG9vay5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlfSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiMWVlMjQ0MTcxMmM1NmY5ZTFlMzQiLCJzY29wZWRLZXlTZWNyZXQiOiIzMWU5NzU2MTVjMTRlMGYzODc1ODY0MmE1ZjljNzg5MzA4YzdiMTFhNGQ2MmQxYWVkYzFmZjliMmJkNjUwOWZkIiwiaWF0IjoxNjQ3MzU5NDMwfQ.rTCbOB-l3LBxIP10sMRE12rrsMcXDLIom9m0UW_H9BY
--- www/src/Minter.js
+++ www/src/Minter.js
@@ -1,25 +1,62 @@
 import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected, //import here
+  mintNFT
+} from "./utils/interact.js";
 
 const Minter = (props) => {
 
   //State variables
-  const [walletAddress, setWallet] = useState("");
-  const [status, setStatus] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [url, setURL] = useState("");
+  const [walletAddress, setWallet] = useState(""); // a string that stores the user's wallet address
+  const [status, setStatus] = useState(""); //a string that contains a message to display at the bottom of the UI
+  const [name, setName] = useState(""); //a string that stores the NFT's name
+  const [description, setDescription] = useState(""); //a string that stores the NFT's description
+  const [url, setURL] = useState("");  //a string that is a link to the NFT's digital asset
  
   useEffect(async () => { //TODO: implement
-    
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status); 
+
+    addWalletListener();
   }, []);
 
   const connectWalletPressed = async () => { //TODO: implement
-   
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
-  const onMintPressed = async () => { //TODO: implement
-    
+  const onMintPressed = async () => {
+    const { status } = await mintNFT(url, name, description);
+    setStatus(status);
   };
+
+  function addWalletListener() {  //in order to update our UI when our wallet's state changes
+    if (window.ethereum) {       //Metamask is installed
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
 
   return (
     <div className="Minter">
